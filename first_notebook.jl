23166fca967244efa3bb01e8dfe638eeccf91142
rvanco <98898021+rvanco@users.noreply.github.com>
2022-07-04 12:09:56 +0200

    Basic Pluto notebook
    no package, just learning basic

---
+++ first_notebook.jl
@@ -0,0 +1,85 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3fb2a702-f485-4972-bec0-1f4e017409ba
+md"# first pluto notebook"
+
+# ╔═╡ 3ba7e8dc-f923-4c19-bf63-3bf3dbd45698
+md"## _Wrap in a **begin ... end** cell :_ "
+
+# ╔═╡ 46799ee7-af88-475b-97e9-c690cc6539fb
+begin
+	a = 3
+	b = 4
+	c = a + b
+	print(c)
+end
+
+# ╔═╡ 052077bd-2acb-47ff-8344-d2ef5ddb7b26
+md"## _Put into multiple cell :_"
+
+# ╔═╡ 7e8116e4-732e-46b0-9d37-c85503abb711
+d = 3
+
+# ╔═╡ cbc9ef07-73ff-4222-86de-a68e7ff3354c
+e = 4
+
+# ╔═╡ 2e58a753-b75f-481f-9ef8-c50d7bbcaee2
+f = d + e
+
+# ╔═╡ 9937067e-65c9-411e-ba4b-81e4cc38d023
+print(f)
+
+# ╔═╡ cd92549d-f403-4957-b694-8ea6024f9910
+
+
+# ╔═╡ a177b195-5652-410f-ab08-6efaed66e0a4
+
+
+# ╔═╡ e5e0ffb9-8d49-47db-a7a4-38d6866f24dc
+
+
+# ╔═╡ 8014cb1b-ae69-4ec4-b067-f3b8aee1f549
+
+
+# ╔═╡ facf296d-c3cb-4fe1-9ffe-096fe34907e2
+
+
+# ╔═╡ 82d8c1a6-bc1b-4cd8-aef2-e698ae7d73e6
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.3"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─3fb2a702-f485-4972-bec0-1f4e017409ba
+# ╟─3ba7e8dc-f923-4c19-bf63-3bf3dbd45698
+# ╠═46799ee7-af88-475b-97e9-c690cc6539fb
+# ╟─052077bd-2acb-47ff-8344-d2ef5ddb7b26
+# ╠═7e8116e4-732e-46b0-9d37-c85503abb711
+# ╠═cbc9ef07-73ff-4222-86de-a68e7ff3354c
+# ╠═2e58a753-b75f-481f-9ef8-c50d7bbcaee2
+# ╠═9937067e-65c9-411e-ba4b-81e4cc38d023
+# ╠═cd92549d-f403-4957-b694-8ea6024f9910
+# ╠═a177b195-5652-410f-ab08-6efaed66e0a4
+# ╠═e5e0ffb9-8d49-47db-a7a4-38d6866f24dc
+# ╠═8014cb1b-ae69-4ec4-b067-f3b8aee1f549
+# ╠═facf296d-c3cb-4fe1-9ffe-096fe34907e2
+# ╠═82d8c1a6-bc1b-4cd8-aef2-e698ae7d73e6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
