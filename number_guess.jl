618ca656a5ad8608299c45f96160d2f9668d29b5
rvanco <98898021+rvanco@users.noreply.github.com>
2022-07-06 14:08:58 +0200

    adding comments and hint probability

--- number_guess.jl
+++ number_guess.jl
@@ -1,17 +1,32 @@
 function play_number_guess()
-    total_number = 25
-    target_number = rand(1:total_number)
-
+    print("Please enter the maximal number : ")
+    max_number = parse(Int64, readline())  
+    # readline() let the player select a value
+    # parse(Int64, something) change the type of something (here a string) to an Integer
+    target_number = rand(1:max_number)
+    # choose a rendom number between 1 and the number chose just before
     guess = 0
+    # set up a guess that is false to enter the while loop
 
-    while guess != target_number
-        print("Please guess a number between 1 and ", total_number, " : ")
+    while guess != target_number    # while the condition "guess different from target number" is true the loop continue
+        print("\n\nPlease guess a number between 1 and ", max_number, " : ")
         guess = parse(Int64, readline())
-        if guess != target_number
-            print("\n")
-            print("Your guess is wrong, try again ;) \n")
+        # let the player select a value and change it to an Integer
+
+        if guess != target_number   # if the guess is different from the target the game tell it to the player
+            print("Your guess is wrong, try again ;) ")
+            hint = rand(1:5)
+            # create a random value, if equal 1 the game give a hint to the player, here 1 in 5 chance to get a hint
+
+            if hint == 1
+                if guess < target_number # if the guess is lower than the target the game tell it
+                    print("\nHint : you should seek higher number ")
+
+                else # if the guess is higher than the target the game tell it
+                    print("\nHint : you should seek lower number ")
+                end
+            end
         end
-    end
-    print("\n")
-    print("You guess correctly")
+    end  # when "guess == target_number" the while loop end and the game is finished :
+    print("\nYou guess correctly")
 end
